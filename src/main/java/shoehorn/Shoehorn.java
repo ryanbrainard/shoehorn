8aebbb50b1a15c6216d94239f08d31d8304b39e9
Ryan Brainard <brainard@heroku.com>
2012-03-14 10:40:25 -0700

    Basic envvar to sysprop setting

--- src/main/java/shoehorn/Shoehorn.java
+++ src/main/java/shoehorn/Shoehorn.java
@@ -1,11 +1,17 @@
 package shoehorn;
 
+import java.util.Map;
+
 /**
  * @author Ryan Brainard
  */
 public class Shoehorn {
 
     public static void premain(String agentArgs) {
-        System.out.println("***************PRE-MAIN***************");
+        System.out.println("Registering Environment Variables as Java System Properties");
+        
+        for (Map.Entry<String, String> envVar : System.getenv().entrySet()) {
+            System.setProperty(envVar.getKey(), envVar.getValue());
+        }
     }
 }
