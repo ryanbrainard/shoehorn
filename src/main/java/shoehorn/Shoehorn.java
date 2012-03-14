e71fdf21d23cad33f146595dcab25e477538050c
Ryan Brainard <brainard@heroku.com>
2012-03-14 10:30:14 -0700

    init basic premain

---
+++ src/main/java/shoehorn/Shoehorn.java
@@ -0,0 +1,11 @@
+package shoehorn;
+
+/**
+ * @author Ryan Brainard
+ */
+public class Shoehorn {
+
+    public static void premain(String agentArgs) {
+        System.out.println("***************PRE-MAIN***************");
+    }
+}
